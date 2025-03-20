bdc14d359e4521afdfdfd30e68a2affcdabefc74
Gilles ORSINI <gilles.orsini@gmail.com>
2025-03-20 22:29:23 +0100

    function compute application with regexp

--- input-func.js
+++ input-func.js
@@ -1,12 +1,12 @@
 return Enumerable
-        .from(VM)
-        .leftJoin(patch_policies,
-            outer => outer.vm,
-            inner => inner.application,
-            (outer, inner) => {
-                return {
-                    vm: outer.vm,
-                    cve: outer.cve,
-                    patchPolicy: inner?.patchPolicy ?? ''
-                }
-            }).toArray();
+    .from(VM)
+    .leftJoin(patch_policies,
+        outer => outer.vm.replace(/([0-9]+p?(.zone.local)?)/g, ''),
+        inner => inner.application,
+        (outer, inner) => {
+            return {
+                vm: outer.vm,
+                cve: outer.cve,
+                patchPolicy: inner?.patchPolicy ?? ''
+            }
+        }).toArray();
