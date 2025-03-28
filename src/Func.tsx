183a8e3a9c492f9f1e6f2aa5e366743b69e8c830
Gilles ORSINI <gilles.orsini@gmail.com>
2025-03-28 22:37:45 +0100

    add maximize

--- src/Func.tsx
+++ src/Func.tsx
@@ -1,9 +1,14 @@
-import FilePicker from '@ihatecode/react-file-picker';
-import { Button, IconButton, Paper, Stack } from '@mui/material';
+import { useState } from 'react';
+import { Button, IconButton, Paper, Stack, Tooltip } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import FilePicker from '@ihatecode/react-file-picker';
 import { saveAs } from "file-saver";
 
 export function Func({ funcBody, onFuncBodyChange, onClose }) {
+
+    const [isMaximize, setMaximize] = useState(false);
 
     const onChange = (e) => {
         onFuncBodyChange(e.target.value);
@@ -27,6 +32,23 @@ export function Func({ funcBody, onFuncBodyChange, onClose }) {
         saveAs(blob, filename);
     };
 
+    const maximizeStyle = isMaximize ? {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#f0f0f0",
+        zIndex: 1000,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    } : {
+        height: "100%",
+        width: "30em",
+        margin: "4px"
+    };
+
     return <Paper >
         <Stack sx={{ height: "100%" }}>
             <Stack direction="row" spacing={1} >
@@ -36,12 +58,16 @@ export function Func({ funcBody, onFuncBodyChange, onClose }) {
                     onChange={(files) => onUpload(files)}><Button>Open</Button>
                 </FilePicker>
                 <Button onClick={onDownload}>Save</Button>
-                <IconButton onClick={onClose}><CloseIcon /></IconButton>
+                <Tooltip title="Full screen"><IconButton onClick={() => setMaximize(true)}><FullscreenIcon /></IconButton></Tooltip>
+                <Tooltip title="Close panel"><IconButton onClick={onClose}><CloseIcon /></IconButton></Tooltip>
             </Stack>
             <textarea value={funcBody} onChange={onChange}
-                style={{ margin: "4px", height: "100%", width: "30em",
-                whiteSpace: "nowrap", fontFamily: "monospace", resize: 'none' }}
-                ></textarea>
+                style={{
+                    ...maximizeStyle,
+                    whiteSpace: "nowrap", fontFamily: "monospace", resize: 'none'
+                }}
+            ></textarea>
+            {isMaximize && <Tooltip title="Exit full screen"><IconButton sx={{ position: "absolute", top: "0", right: "0", zIndex: 1001, color: "black" }} onClick={() => setMaximize(false)}><FullscreenExitIcon /></IconButton></Tooltip>}
         </Stack>
     </Paper>
 
